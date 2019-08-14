77ef807cddd373ae51321d17e873051f7499096a
hunan <hunan@tigerft.com>
2019-08-14 19:49:53 +0800

    inint

---
+++ app/src/main/java/com/hn/test/librarytest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.hn.test.librarytest
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.demo.kotlin.mylibrary.EntranceActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_jump.setOnClickListener(this);
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_jump->{
+                Toast.makeText(this,"点击", Toast.LENGTH_LONG).show();
+                var intent = Intent(this,EntranceActivity::class.java);
+                startActivity(intent);
+//                val intent = Intent(this, EntranceActivity::class.java)
+//                startActivity(intent)
+            }
+        }
+    }
+}
